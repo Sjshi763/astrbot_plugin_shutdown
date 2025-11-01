46f327ceec461a0844eada1cf9da99cfb6ebe3f4
圣达生物多 <qq3258819795@163.com>
2025-11-01 12:26:03 +0800

    good

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from astrbot.api import logger # 使用 astrbot 提供的 logger 接口
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 
-@filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 获取消息的纯文本内容
-        logger.info("触发hello world指令!")
-        yield event.plain_result(f"Hello, {user_name}!") # 发送一条纯文本消息
-        logger.info()
+@filter.permission_type(filter.PermissionType.ADMIN)
+@filter.command("StopServeStart")
+async def StopServeStart(self, event: AstrMessageEvent,StartTime: str):
+    '''开始时间 以发送14:10为例，会在每天14：10拒绝服务''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
+    logger.debug(f"收到开始消息,尝试设置在{StartTime}，停止LLM")
+
+    yield event.plain_result(f"Hello, !") # 发送一条纯文本消息
+
+@filter.permission_type(filter.PermissionType.ADMIN)
+@filter.command("StopServeEnd")
+async def StopServeEnd(self, event: AstrMessageEvent):
+    '''''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
+    user_name = event.get_sender_name()
+    message_str = event.message_str # 获取消息的纯文本内容
+    logger.info("收到开始消息，LLM")
+    yield event.plain_result(f"Hello, {user_name}!") # 发送一条纯文本消息
+    logger.info()
+
+@filter.event_message_type(filter.EventMessageType.ALL)
