c6ec4f5c6aa1080fb96714adc20263493003b0ab
圣达生物多 <qq3258819795@163.com>
2025-11-01 12:38:16 +0800

    update

--- main.py
+++ main.py
@@ -6,17 +6,13 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 async def StopServeStart(self, event: AstrMessageEvent,StartTime: str):
     '''开始时间 以发送14:10为例，会在每天14：10拒绝服务''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
     logger.debug(f"收到开始消息,尝试设置在{StartTime}，停止LLM")
-
+    
     yield event.plain_result(f"Hello, !") # 发送一条纯文本消息
 
 @filter.permission_type(filter.PermissionType.ADMIN)
 @filter.command("StopServeEnd")
-async def StopServeEnd(self, event: AstrMessageEvent):
-    '''''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
-    user_name = event.get_sender_name()
-    message_str = event.message_str # 获取消息的纯文本内容
-    logger.info("收到开始消息，LLM")
-    yield event.plain_result(f"Hello, {user_name}!") # 发送一条纯文本消息
-    logger.info()
+async def StopServeEnd(self, event: AstrMessageEvent,EndTime: str):
+    '''结束时间 以发送14:10为例，会在每天14：10开始服务''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
+    logger.debug(f"收到开始消息,尝试设置在{EndTime}，停止LLM")
 
 @filter.event_message_type(filter.EventMessageType.ALL)
